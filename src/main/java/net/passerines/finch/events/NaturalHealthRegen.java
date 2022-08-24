87a0d385f65be3a00e5b3475eaf7dc77ad3a1cde
funkykangaroo111 <73447741+funkykangaroo111@users.noreply.github.com>
2022-08-23 20:09:47 -0700

    Update NaturalHealthRegen.java
    Tried to do things

--- src/main/java/net/passerines/finch/events/NaturalHealthRegen.java
+++ src/main/java/net/passerines/finch/events/NaturalHealthRegen.java
@@ -1,4 +1,21 @@
 package net.passerines.finch.events;
 
+import net.passerines.finch.FinchElementalDamage;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import static net.passerines.finch.players.PlayerMap.PLAYERS;
+
 public class NaturalHealthRegen {
+    private Player player;
+
+    public NaturalHealthRegen(){
+
+        if(){
+            Bukkit.getScheduler().scheduleSyncRepeatingTask(FinchElementalDamage.inst(), ()->{
+                PLAYERS.setHealth(PLAYERS.get(player).getHealth() + PLAYERS.get(player).getHealth()*0.01);
+            }, 0, 100);
+        }
+    }
 }
