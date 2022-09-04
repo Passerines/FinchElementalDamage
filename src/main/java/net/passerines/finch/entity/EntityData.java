ea7298f45c8f39526c67d858a9ed793c5c34e18d
SinStuds <sonkun9001@gmail.com>
2022-09-04 13:05:37 -0700

    Revert "Changed EntityData setdamage from 5 to 15"
    This reverts commit 890441f5ba4b16dec30ba5138c1364d3951cd146.

--- src/main/java/net/passerines/finch/entity/EntityData.java
+++ src/main/java/net/passerines/finch/entity/EntityData.java
@@ -7,6 +7,7 @@ import net.passerines.finch.items.FinchWeapon;
 import net.passerines.finch.items.ItemManager;
 import net.passerines.finch.util.Util;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class EntityData {
@@ -27,7 +28,7 @@ public class EntityData {
     public void reset() {
         setHealthMax(100);
         setDefense(10);
-        setDamage(15);
+        setDamage(5);
     }
 
     public double getHealth() {
