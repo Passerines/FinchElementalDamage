8ff00e74fc7ae3445200fb732fe235f2db9e97f5
funkykangaroo111 <73447741+funkykangaroo111@users.noreply.github.com>
2022-09-15 20:21:58 -0700

    Update CrescentKnives.java

--- src/main/java/net/passerines/finch/items/weapons/ranged/CrescentKnives.java
+++ src/main/java/net/passerines/finch/items/weapons/ranged/CrescentKnives.java
@@ -36,11 +36,9 @@ public class CrescentKnives extends FinchWeapon implements Listener {
         PlayerData vPlayer = PlayerMap.PLAYERS.get(player);
         ItemStack item = player.getInventory().getItemInMainHand();
         if(event.getAction().isRightClick() || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-            if(id.equals(Util.getId(item))) {
-                new ThrowBlade(id, player, Particle.CRIT_MAGIC, DARK, 2, vPlayer.getDamage());
+                new ThrowBlade(id, player, Particle.CRIT_MAGIC, DARK, 1, vPlayer.getDamage());
                 cd.add(player, 20);
                 Util.log("Thrown Crescent Knives");
-            }
         }
     }
 
