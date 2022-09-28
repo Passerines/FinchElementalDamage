3c541475ab67b6f381c64b758dbd9fe8fbf54b40
funkykangaroo111 <73447741+funkykangaroo111@users.noreply.github.com>
2022-09-27 18:21:54 -0700

    Fixed damage on entites

--- src/main/java/net/passerines/finch/events/handler/DamageDisplayer.java
+++ src/main/java/net/passerines/finch/events/handler/DamageDisplayer.java
@@ -1,6 +1,8 @@
 package net.passerines.finch.events.handler;
 
 import net.passerines.finch.FinchElementalDamage;
+import net.passerines.finch.entity.EntityData;
+import net.passerines.finch.entity.EntityMap;
 import net.passerines.finch.events.ElementalDamageEvent;
 import net.passerines.finch.players.PlayerData;
 import net.passerines.finch.players.PlayerMap;
@@ -10,6 +12,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -23,11 +27,18 @@ public class DamageDisplayer implements Listener {
     @EventHandler
     public void onDamage(ElementalDamageEvent event){
         Entity victim = event.getVictim();
+        double finalDamage = event.getDamage();
+        double damage = 0;
         if(!(victim instanceof ArmorStand)) {
+            if(victim instanceof Player){
+                PlayerData vPlayerData = PlayerMap.PLAYERS.get(victim);
+                damage = (int) ((finalDamage - (finalDamage * (vPlayerData.getDefense() / (vPlayerData.getDefense() + 500.0))) * event.getElement().getElementalMultiplier()));
+            }
+            else if(victim instanceof LivingEntity){
+                EntityData vEntityData = EntityMap.ENTITIES.get(victim);
+                damage = (finalDamage - finalDamage * (vEntityData.getDefense() / (vEntityData.getDefense() + 500.0)));
+            }
             Location loc = victim.getLocation().add(Util.rand(-1.0, 1.0), Util.rand(-1.0, 1.0), Util.rand(-1.0, 1.0));
-            PlayerData vPlayerData = PlayerMap.PLAYERS.get((victim));
-            double finalDamage = event.getDamage();
-            double damage = (int) ((finalDamage - (finalDamage * (vPlayerData.getDefense() / (vPlayerData.getDefense() + 500.0))) * event.getElement().getElementalMultiplier()));
             ArmorStand damageDisplay = loc.getWorld().spawn(loc, ArmorStand.class);
             damageDisplay.getPersistentDataContainer().set(Util.getNamespacedKey("remove"), PersistentDataType.BYTE, (byte) 1);
             damageDisplay.setInvisible(true);
