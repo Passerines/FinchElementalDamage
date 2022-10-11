39b4d02326514a218021dd82c19e19dd97b8d2ab
funkykangaroo111 <73447741+funkykangaroo111@users.noreply.github.com>
2022-10-10 20:10:49 -0700

    typo

--- src/main/java/net/passerines/finch/cmds/ItemGiveCommand.java
+++ src/main/java/net/passerines/finch/cmds/ItemGiveCommand.java
@@ -41,7 +41,7 @@ public class ItemGiveCommand implements CommandExecutor, TabCompleter {
                         if(args.length > 1){
                             try {
                                 //if args is /itemgive "itemname" amount , give amount item to commandSender
-                                amount = Integer.parseInt(args[1]);
+                                amount = Integer.parseInt(args[3]);
                                 if(player.getInventory().firstEmpty() < 0){
                                     Chat.sendTitle(player, "&cInventory Full");
                                 }
