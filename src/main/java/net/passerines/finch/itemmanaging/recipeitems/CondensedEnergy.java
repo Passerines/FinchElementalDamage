a83a04477232d0220182e8b0a3e011f9bcf5d63f
funkykangaroo111 <73447741+funkykangaroo111@users.noreply.github.com>
2022-10-15 10:38:12 -0700

    Added items to itemmanager and changed condensedEnergy head

--- src/main/java/net/passerines/finch/itemmanaging/recipeitems/CondensedEnergy.java
+++ src/main/java/net/passerines/finch/itemmanaging/recipeitems/CondensedEnergy.java
@@ -26,11 +26,11 @@ public class CondensedEnergy extends FinchItem implements FinchCraftableItem {
         ItemStack item = new ItemStack(Material.PLAYER_HEAD);
         ItemMeta itemMeta = item.getItemMeta();
         SkullMeta skullItemMeta = (SkullMeta) item.getItemMeta();
+        skullItemMeta.setOwningPlayer(Bukkit.getOfflinePlayer("mochhi"));
         itemMeta.displayName(Chat.formatC("&bCondensed Energy"));
         ArrayList<Component> lore = new ArrayList<>();
         lore.add(Chat.formatC("&7Enough Energy To Make A Nuclear Bomb!"));
         itemMeta.lore(lore);
-        skullItemMeta.setOwningPlayer(Bukkit.getOfflinePlayer("ianjezhua"));
         item.setItemMeta(itemMeta);
         item.setItemMeta(skullItemMeta);
         return writeId(item);
